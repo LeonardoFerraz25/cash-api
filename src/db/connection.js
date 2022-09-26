5522c150ab8b47ae38a98366ae43d12df838f77a
Leonardo Ferraz Novaes <leonardo.ferrazn@gmail.com>
2022-09-26 18:22:47 -0300

    refator: variaveis de ambiente na conexão com o banco de dados

--- src/db/connection.js
+++ src/db/connection.js
@@ -2,14 +2,14 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const connection = mysql.createPool({
-  host: '0.0.0.0',
-  port: 33060,
-  user: 'root',
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+  waitForConnections: process.env.MYSQL_WAIT_FOR_CONNECTIONS,
+  connectionLimit: process.env.MYSQL_CONNECTION_LIMIT,
+  queueLimit: process.env.MYSQL_QUEUE_LIMIT,
 });
 
 module.exports = connection;
