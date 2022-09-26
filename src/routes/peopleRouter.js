40947e49802cfe3979b501a8fee2f38d7269eef8
Leonardo Ferraz Novaes <leonardo.ferrazn@gmail.com>
2022-09-26 16:09:00 -0300

    feat: add rotas get no endpoint '/people'

--- src/routes/peopleRouter.js
+++ src/routes/peopleRouter.js
@@ -8,22 +8,43 @@ people.post('/', async(req, res) => {
   const person = req.body;
 
   try {
-    
     const [result] = await peopleDb.insert(person);
-
     return res.status(201).json({
       "message": `Pessoa cadastrada com sucesso com o id ${result.insertId}`
     })
-
   } catch(err) {
-
     console.log(err)
     return res.status(500).json({
       "message": "Ocorreu um erro ao cadastrar uma pessoa"
     })
-
   }
-  
+
 })
+
+people.get('/', async (_req, res) => {
+  try {
+    const [result] = await peopleDb.findAll();
+    return res.status(200).json(result);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ "message": err.sqlMessage });
+  }
+});
+
+people.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const [[result]] = await peopleDb.findById(id);
+    if (result) {
+      return res.status(200).json(result);
+    } else {
+      return res.status(404).json({ "message": 'Pessoa não encontrada' });
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ "message": err.sqlMessage });
+  }
+});
+
 
 module.exports = people;
