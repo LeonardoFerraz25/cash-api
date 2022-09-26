171164862601b78e11b1ef98be02311e7b6ee3b0
Leonardo Ferraz Novaes <leonardo.ferrazn@gmail.com>
2022-09-26 14:21:25 -0300

    feat(docker): criando docker-compose com imagem do mysql e variaveis de ambiente

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
-const app = require('./app')
-const port = 3001
+const app = require('./app');
+require('dotenv').config();
+
+const port = process.env.NODE_DOCKER_PORT || 3001
 
 app.listen(port, () => console.log(`server running on port ${port}!`))
